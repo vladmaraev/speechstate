41f5d1e19005a5952509f26e298aa55b1d5d15e5
Vladislav Maraev <vlad@maraev.me>
2022-11-29 10:45:09 +0100

    Update to embeddable version and latest xstate

--- src/dmColourChanger.ts
+++ src/dmColourChanger.ts
@@ -1,63 +1,67 @@
 import { MachineConfig, send, Action } from "xstate";
 
-
 const sayColour: Action<SDSContext, SDSEvent> = send((context: SDSContext) => ({
-    type: "SPEAK", value: `Repainting to ${context.recResult[0].utterance}`
-}))
+  type: "SPEAK",
+  value: `Repainting to ${context.recResult[0].utterance}`,
+}));
 
 function say(text: string): Action<SDSContext, SDSEvent> {
-    return send((_context: SDSContext) => ({ type: "SPEAK", value: text }))
+  return send((_context: SDSContext) => ({ type: "SPEAK", value: text }));
 }
 
-export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = ({
-    initial: 'idle',
-    states: {
-        idle: {
-            on: {
-                CLICK: 'init'
-            }
-        },
-        init: {
-            on: {
-                TTS_READY: 'welcome',
-                CLICK: 'welcome'
-            }
-        },
+export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
+  initial: "idle",
+  states: {
+    idle: {
+      on: {
+        CLICK: "init",
+      },
+    },
+    init: {
+      on: {
+        TTS_READY: "welcome",
+        CLICK: "welcome",
+      },
+    },
 
-        welcome: {
-            initial: 'prompt',
-            on: {
-                RECOGNISED: [
-                    { target: 'stop', cond: (context) => context.recResult[0].utterance === 'Stop.' },
-                    { target: 'repaint' }],
-                TIMEOUT: '..',
-            },
-            states: {
-                prompt: {
-                    entry: say("Tell me the colour"),
-                    on: { ENDSPEECH: 'ask' }
-                },
-                ask: {
-                    entry: send('LISTEN'),
-                },
-            }
+    welcome: {
+      initial: "prompt",
+      on: {
+        RECOGNISED: [
+          {
+            target: "stop",
+            cond: (context) => context.recResult[0].utterance === "Stop.",
+          },
+          { target: "repaint" },
+        ],
+        TIMEOUT: "..",
+      },
+      states: {
+        prompt: {
+          entry: say("Tell me the colour"),
+          on: { ENDSPEECH: "ask" },
         },
-        stop: {
-            entry: say("Ok"),
-            always: 'init'
+        ask: {
+          entry: send("LISTEN"),
+        },
+      },
+    },
+    stop: {
+      entry: say("Ok"),
+      always: "init",
+    },
+    repaint: {
+      initial: "prompt",
+      states: {
+        prompt: {
+          entry: sayColour,
+          on: { ENDSPEECH: "repaint" },
         },
         repaint: {
-            initial: 'prompt',
-            states: {
-                prompt: {
-                    entry: sayColour,
-                    on: { ENDSPEECH: 'repaint' }
-                },
-                repaint: {
-                    entry: 'changeColour',
-                    always: '#root.dm.welcome'
-                }
-            }
-        }
-    }
-})
+          entry: "changeColour",
+          always: "#root.dm.welcome",
+        },
+      },
+    },
+  },
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,54 +1,59 @@
 /// <reference types="react-scripts" />
 
-declare module 'react-speech-kit';
-declare module 'web-speech-cognitive-services/lib/SpeechServices/TextToSpeech';
-declare module 'web-speech-cognitive-services/lib/SpeechServices/SpeechToText';
+declare module "react-speech-kit";
+declare module "web-speech-cognitive-services/lib/SpeechServices/TextToSpeech";
+declare module "web-speech-cognitive-services/lib/SpeechServices/SpeechToText";
 
 interface Hypothesis {
-    "utterance": string;
-    "confidence": number
+  utterance: string;
+  confidence: number;
 }
 
 interface MySpeechSynthesisUtterance extends SpeechSynthesisUtterance {
-    new(s: string);
+  new (s: string);
 }
 
 interface MySpeechRecognition extends SpeechRecognition {
-    new(s: string);
+  new (s: string);
+}
+
+interface Settings {
+  ttsVoice: string;
+  ttsLexicon: string;
+  asrLanguage: string;
+  azureKey: string;
 }
 
 interface SDSContext {
-    asr: SpeechRecognition;
-    tts: SpeechSynthesis;
-    voice: SpeechSynthesisVoice;
-    ttsUtterance: MySpeechSynthesisUtterance;
-    recResult: Hypothesis[];
-    hapticInput: string;
-    nluData: any;
-    ttsAgenda: string;
-    query: string;
-    snippet: string;
-    sessionId: string;
-    tdmAll: any;
-    tdmUtterance: string;
-    tdmPassivity: number;
-    tdmActions: any;
-    tdmVisualOutputInfo: any;
-    tdmExpectedAlternatives: any;
-    azureAuthorizationToken: string;
-    audioCtx: any;
+  parameters: Parameters;
+  asr: SpeechRecognition;
+  tts: SpeechSynthesis;
+  voice: SpeechSynthesisVoice;
+  ttsUtterance: MySpeechSynthesisUtterance;
+  recResult: Hypothesis[];
+  hapticInput: string;
+  nluData: any;
+  ttsAgenda: string;
+  query: string;
+  snippet: string;
+  sessionId: string;
+  azureAuthorizationToken: string;
+  audioCtx: any;
+
+  gameCount: number;
+  winCount: number;
 }
 
 type SDSEvent =
-    | { type: 'TTS_READY' }
-    | { type: 'TTS_ERROR' }
-    | { type: 'CLICK' }
-    | { type: 'SELECT', value: any }
-    | { type: 'SHOW_ALTERNATIVES' }
-    | { type: 'STARTSPEECH' }
-    | { type: 'RECOGNISED' }
-    | { type: 'ASRRESULT', value: Hypothesis[] }
-    | { type: 'ENDSPEECH' }
-    | { type: 'LISTEN' }
-    | { type: 'TIMEOUT' }
-    | { type: 'SPEAK', value: string };
+  | { type: "TTS_READY" }
+  | { type: "TTS_ERROR" }
+  | { type: "CLICK" }
+  | { type: "SELECT"; value: any }
+  | { type: "SHOW_ALTERNATIVES" }
+  | { type: "STARTSPEECH" }
+  | { type: "RECOGNISED" }
+  | { type: "ASRRESULT"; value: Hypothesis[] }
+  | { type: "ENDSPEECH" }
+  | { type: "LISTEN" }
+  | { type: "TIMEOUT" }
+  | { type: "SPEAK"; value: string };
