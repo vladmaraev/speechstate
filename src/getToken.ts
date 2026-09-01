e02303f219ecea3527777c96528dae059a984c25
Vladislav Maraev <vladislav.maraev@gu.se>
2026-09-01 11:29:21 +0200

    Support authorization header for Azure proxy (#30)
    This change adds a support for proxies for azure keys which require
    additional authorisation. This is enabled by providing an object
    `AzureSpeechTokenProxyCredentials` as credentials in `Settings`.
    
    If the proxy doesn't require authorisation, it is recommended to use
    the same object but omit the `key` field, instead of using bare string
    URL which will be eventually deprecated.
    
    Additionally, CI now is moved to trusted publishing (OIDC).

--- src/getToken.ts
+++ src/getToken.ts
@@ -1,16 +1,42 @@
 import { fromPromise } from "xstate";
+import {
+  AzureSpeechCredentials,
+  AzureSpeechTokenProxyCredentials,
+} from "./types";
 
-export const getToken = fromPromise(async ({ input }: { input: any }) => {
-  if (typeof input.credentials === "string") {
-    return fetch(new Request(input.credentials)).then((data) => data.text());
-  } else {
-    return fetch(
-      new Request(input.credentials.endpoint, {
-        method: "POST",
-        headers: {
-          "Ocp-Apim-Subscription-Key": input.credentials.key,
-        },
-      }),
-    ).then((data) => data.text());
-  }
-});
+export const getToken = fromPromise(
+  async ({
+    input,
+  }: {
+    input: {
+      credentials:
+        | AzureSpeechTokenProxyCredentials
+        | AzureSpeechCredentials
+        | string;
+    };
+  }) => {
+    if (typeof input.credentials === "string") {
+      return fetch(new Request(input.credentials)).then((data) => data.text());
+    } else {
+      if ("proxyUrl" in input.credentials) {
+        return fetch(
+          new Request(
+            input.credentials.proxyUrl,
+            input.credentials.key && {
+              headers: { Authorization: `Bearer ${input.credentials.key}` },
+            },
+          ),
+        ).then((data) => data.text());
+      } else {
+        return fetch(
+          new Request(input.credentials.endpoint, {
+            method: "POST",
+            headers: {
+              "Ocp-Apim-Subscription-Key": input.credentials.key,
+            },
+          }),
+        ).then((data) => data.text());
+      }
+    }
+  },
+);
